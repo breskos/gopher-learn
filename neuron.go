03cb416219f22d227b41c5af649581d35b57a096
Alexander Bresk <abresk@me.com>
2020-06-20 12:26:53 +0200

    add comments for source files

--- neuron.go
+++ neuron.go
@@ -1,5 +1,6 @@
 package neural
 
+// Neuron holds the data a neuron needs
 type Neuron struct {
 	OutSynapses        []*Synapse
 	InSynapses         []*Synapse         `json:"-"`
@@ -7,26 +8,28 @@ type Neuron struct {
 	Out                float64            `json:"-"`
 }
 
+// NewNeuron creates a new neuron
 func NewNeuron() *Neuron {
 	return &Neuron{}
 }
 
+// SynapseTo creates a new synapse to a neuron
 func (n *Neuron) SynapseTo(nTo *Neuron, weight float64) {
 	NewSynapseFromTo(n, nTo, weight)
 }
 
+// SetActivationFunction sets the activation function for the neuron
 func (n *Neuron) SetActivationFunction(aFunc ActivationFunction) {
 	n.ActivationFunction = aFunc
 }
 
+// Calculate calculates the actual neuron activity
 func (n *Neuron) Calculate() {
 	var sum float64
 	for _, s := range n.InSynapses {
 		sum += s.Out
 	}
-
 	n.Out = n.ActivationFunction(sum)
-
 	for _, s := range n.OutSynapses {
 		s.Signal(n.Out)
 	}
--- synapse.go
+++ synapse.go
@@ -1,24 +1,26 @@
 package neural
 
-func NewSynapse(weight float64) *Synapse {
-	return &Synapse{Weight: weight}
-}
-
-func NewSynapseFromTo(from, to *Neuron, weight float64) *Synapse {
-	syn := NewSynapse(weight)
-
-	from.OutSynapses = append(from.OutSynapses, syn)
-	to.InSynapses = append(to.InSynapses, syn)
-
-	return syn
-}
-
+// Synapse holds the synapse structure
 type Synapse struct {
 	Weight float64
 	In     float64 `json:"-"`
 	Out    float64 `json:"-"`
 }
 
+// NewSynapse creates a new synapse
+func NewSynapse(weight float64) *Synapse {
+	return &Synapse{Weight: weight}
+}
+
+// NewSynapseFromTo creates a new synapse from neuron to neuron
+func NewSynapseFromTo(from, to *Neuron, weight float64) *Synapse {
+	syn := NewSynapse(weight)
+	from.OutSynapses = append(from.OutSynapses, syn)
+	to.InSynapses = append(to.InSynapses, syn)
+	return syn
+}
+
+// Signal activates the Synapse with an input value
 func (s *Synapse) Signal(value float64) {
 	s.In = value
 	s.Out = s.In * s.Weight
