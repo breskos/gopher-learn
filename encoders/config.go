5f0eafd0fa3a6a8c3286dc72fedef2eb815c8c8c
Alexander Bresk <alexander@bresk.net>
2021-06-03 18:09:35 +0200

    :sparkles: add parameters to encoder config

--- encoders/config.go
+++ encoders/config.go
@@ -4,26 +4,38 @@ type EncoderConfig struct {
 	DelimiterToken    string
 	DimToSamplesRatio float64
 	// Decision heuristics
-	FloatReducerThreshold   int
-	TopicModelMinDelimiters int
-	NGramsMaxTokens         int
-	DictionaryMaxEntries    int
-	DictionaryMaxDelimiters int
+	FloatReducerThreshold     int
+	TopicModelMinDelimiters   int
+	NGramsMaxTokens           int
+	DictionaryMaxEntries      int
+	DictionaryMaxDelimiters   int
+	SplitDictionaryMaxEntries int
 	// Application settings
-	NGramMaxGrams        int
-	NGramMaxCapacity     int
-	DefaultStringEncoder EncoderType
+	FloatReducerSpearman   float64
+	FloatReducerSkewness   float64
+	FloatReducerZeroValues bool
+	NGramMaxGrams          int
+	NGramMaxCapacity       int
+	NGramCropRatio         float64
+	DefaultStringEncoder   EncoderType
 }
 
 func DefaultConfig() *EncoderConfig {
 	return &EncoderConfig{
-		DelimiterToken:          " ",
-		DimToSamplesRatio:       0.8,
-		FloatReducerThreshold:   40,
-		TopicModelMinDelimiters: 5,
-		NGramsMaxTokens:         20,
-		DictionaryMaxEntries:    50,
-		NGramMaxGrams:           3,
-		DefaultStringEncoder:    StringNGrams,
+		DelimiterToken:            " ",
+		DimToSamplesRatio:         0.8,
+		FloatReducerThreshold:     40,
+		TopicModelMinDelimiters:   5,
+		NGramsMaxTokens:           20,
+		DictionaryMaxEntries:      50,
+		DictionaryMaxDelimiters:   5,
+		SplitDictionaryMaxEntries: 100,
+		FloatReducerSpearman:      0.90,
+		FloatReducerSkewness:      0.90,
+		FloatReducerZeroValues:    true,
+		NGramMaxGrams:             3,
+		NGramMaxCapacity:          100,
+		NGramCropRatio:            0.05,
+		DefaultStringEncoder:      StringNGrams,
 	}
 }
